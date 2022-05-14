7230a0e3afbb4f6dda75f80856407d1800ce03bd
imharvol <imharvol@gmail.com>
2022-05-14 13:44:43 +0200

    Añadir librerias y vistas signin y signup

---
+++ src/routes/signin.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let errors = {}
+</script>
+
+<head>
+  <title>Acceso</title>
+</head>
+
+<main>
+  <form method="post" action="">
+    {JSON.stringify(errors)}
+    <input type="text" placeholder="Nombre de usuario" />
+    <input type="password" placeholder="Contraseña" />
+    <input type="submit" value="Iniciar sesión" />
+  </form>
+</main>
+
+<style>
+</style>
---
+++ src/routes/signup.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let errors = {}
+</script>
+
+<head>
+  <title>Registro</title>
+</head>
+
+<main>
+  <form method="post" action="">
+    {JSON.stringify(errors)}
+    <input type="text" name="username" placeholder="Nombre de usuario" />
+    <input type="password" name="password" placeholder="Contraseña" />
+    <input type="password" name="passwordConfirmation" placeholder="Confirma tu contraseña" />
+    <input type="submit" value="Crear cuenta" />
+  </form>
+</main>
+
+<style>
+</style>
