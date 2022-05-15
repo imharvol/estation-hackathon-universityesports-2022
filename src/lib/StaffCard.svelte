3318dfdf2d7623aeb31468188e9471097006dc67
imharvol <imharvol@gmail.com>
2022-05-15 05:38:48 +0200

    Extract StaffCard component

---
+++ src/lib/StaffCard.svelte
@@ -0,0 +1,56 @@
+<script>
+  export let person = {}
+</script>
+
+<div class="flex-item flex-container">
+  <div>
+    <img
+      class="person-photo"
+      src="/staff/photo/{person.id}"
+      alt="photo of {person.name} {person.surname}"
+    />
+  </div>
+  <div class="person-attributes">
+    <div class="person-attribute name">
+      Nombre: <a href="/staff/{person.id}">{person.name} {person.surname}</a>
+    </div>
+    <div class="person-attribute email">
+      Email: <a href="mailto:{person.email}">{person.email}</a>
+    </div>
+    <div class="person-attribute rol">
+      Rol: {person.role}
+    </div>
+  </div>
+</div>
+
+<style>
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  .flex-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .flex-item {
+    background-color: #f95959;
+    border-radius: 0.5rem;
+    font-size: x-large;
+    margin: 0.5rem;
+    padding: 0.5rem;
+  }
+
+  .person-photo {
+    height: 8vh;
+    max-width: 8vh;
+    border-radius: 50%;
+    margin-right: 1rem;
+  }
+  .person-attribute {
+    display: block;
+  }
+</style>
