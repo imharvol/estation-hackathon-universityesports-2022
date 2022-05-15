eaff38174744cdfdfc39266abb60d40ac149b82a
imharvol <imharvol@gmail.com>
2022-05-15 11:51:47 +0200

    Añadir información la página de inicio

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,42 @@
 <script>
+  import { loop_guard } from 'svelte/internal'
 </script>
 
 <head>
   <title>Inicio | ESTATION</title>
 </head>
 
-<a href="/staff">Personal</a>
-<a href="/reports">Informes</a>
+<main>
+  <h1>Bienvenido de vuelta</h1>
+
+  <img src="/logo.png" alt="logo de estation" />
+
+  <ul>
+    <li><strong>Personal</strong>: Administra tus empleados, sus datos, perfiles y demás</li>
+    <li>
+      <strong>Informes</strong>: Crea y analiza los informes junto con las personas y equipos
+      relacionados
+    </li>
+    <li><strong>Equipos</strong>: Administra los diferentes equipos dentro de tu club</li>
+    <li><strong>Partidos</strong>: Haz un seguimiento de los partidos de tus equipos</li>
+  </ul>
+</main>
 
 <style>
+  * {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: large;
+  }
+
+  main {
+    text-align: center;
+    width: 60vw;
+    margin-inline: auto;
+  }
+
+  h1 {
+    font-size: 8vmin;
+    color: #78c892;
+    font-weight: bold;
+  }
 </style>
